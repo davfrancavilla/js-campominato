eb86f7c3d362a6238d0f1d0bae88aefbe02e6d9c
Davide Francavilla <davfrancavilla@gmail.com>
2020-07-20 17:46:02 +0200

    Gioco funzionante

--- js/script.js
+++ js/script.js
@@ -1,22 +1,74 @@
-var computerNumbers = [];
+var difficulty;
+var userNumbers = [];
 
-// inserisco 16 numeri casuali non ripetuti in un array
-while (computerNumbers.length<17) {
-    n = getRandom(1,100);
-    if (!isRepeated(computerNumbers,n)){
-        computerNumbers.push(n);
+do {
+    difficulty = parseInt(prompt('Segli la difficoltà: 0, 1 o 2'));
+    switch (difficulty) {
+        case 0:
+            createUserArray(100);
+            totalPoints(100);
+            break;
+        case 1:
+            createUserArray(80);
+            totalPoints(80);
+            break
+        case 2:
+            createUserArray(50);
+            totalPoints(50);
+            break
+        default:
+            alert('Scelta non valida');
     }
+} while (difficulty<0 || difficulty>2);
+
+
+function createComputerArray(max){
+    var computerNumbers = [];
+    while (computerNumbers.length<16) {
+        var n = getRandom(1,max);
+        if (!isRepeated(computerNumbers,n)){
+            computerNumbers.push(n);
+        }
+    }
+    return computerNumbers;
+}
+
+
+function createUserArray(max){
+    var length = max - 16;
+    var userNumber;
+    var computerNumbers = createComputerArray(max);
+    while (userNumbers.length<length && !isRepeated(computerNumbers, userNumber)){
+        userNumber = parseInt(prompt('Inserisci un numero compreso tra 1 e ' + max));
+        if (!isNaN(userNumber)){
+            if (!isRepeated(userNumbers,userNumber) && (0<userNumber) && (userNumber<=max)){
+                userNumbers.push(userNumber);
+            } else if (userNumber<1 || userNumber>max) {
+                alert('Il numero deve essere compreso tra 1 e ' + max);
+            } else if (isRepeated(userNumbers,userNumber)) {
+                alert('Non puoi inserire più volte lo stesso numero');
+            }
+        } else {
+            alert('Inserire un numero');
+        }
+    }
+    return userNumbers;
+}
+
+function totalPoints(max){
+    var length = max - 16;
+    if (userNumbers.length<length) {
+        var points = userNumbers.length - 1;
+        alert('Hai perso. Punteggio totale: ' + points + ' punti');
+    } else {
+        alert('Hai vinto');
+    }
+    return 0;
 }
 
 
 
-
-
-
-
-
-
-// fuzione per controllare se un numero è gia presente in un array
+// funzione per controllare se un numero è gia presente in un array
 function isRepeated(a, element){
     var i = 0;
     var check = false;
@@ -24,7 +76,7 @@ function isRepeated(a, element){
         if (a[i] == element) {
             check = true;
         }
-        i++
+        i++;
     }
     return check;
 }

098d335a067a92a49d24745ea2f6993e66164adb
Davide Francavilla <davfrancavilla@gmail.com>
2020-07-20 17:55:26 +0200

    Aggiunti commenti

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var userNumbers = [];
 
 // permetto all'utente di scegliere la difficolta del gioco
 do {
-    difficulty = parseInt(prompt('Segli la difficoltà: 0, 1 o 2'));
+    difficulty = parseInt(prompt('Scegli la difficoltà: 0, 1 o 2'));
     switch (difficulty) {
         case 0:
             createUserArray(100);
@@ -37,17 +37,17 @@ function createComputerArray(max){
 
 // creo l'array con i numeri dell'utente
 function createUserArray(max){
-    var length = max - 16;
+    var maxLength = max - 16;
     var userNumber;
     var computerNumbers = createComputerArray(max);
-    while (userNumbers.length<length && !isRepeated(computerNumbers, userNumber)){
+    while (userNumbers.length<maxLength && !isRepeated(computerNumbers, userNumber)){
         userNumber = parseInt(prompt('Inserisci un numero compreso tra 1 e ' + max));
-        if (!isNaN(userNumber)){
-            if (!isRepeated(userNumbers,userNumber) && (0<userNumber) && (userNumber<=max)){
+        if (!isNaN(userNumber)){ //controllo che il il valore inserito sia un numero
+            if (!isRepeated(userNumbers,userNumber) && (0<userNumber) && (userNumber<=max)){ // controllo che il valore inserito sia compreso tra 0 e il numero massimo ammesso e che non sia già stato inserito
                 userNumbers.push(userNumber);
-            } else if (userNumber<1 || userNumber>max) {
+            } else if (userNumber<1 || userNumber>max) { // avviso l'utente se il numero inserito non è nel range ammesso
                 alert('Il numero deve essere compreso tra 1 e ' + max);
-            } else if (isRepeated(userNumbers,userNumber)) {
+            } else if (isRepeated(userNumbers,userNumber)) { // avviso l'utente se il numero è gia stato inserito
                 alert('Non puoi inserire più volte lo stesso numero');
             }
         } else {
@@ -59,8 +59,8 @@ function createUserArray(max){
 
 // calcolo se l'utente ha vinto o meno e i punti fatti
 function totalPoints(max){
-    var length = max - 16;
-    if (userNumbers.length<length) {
+    var maxLength = max - 16;
+    if (userNumbers.length<maxLength) {
         var points = userNumbers.length - 1;
         alert('Hai perso. Punteggio totale: ' + points + ' punti');
     } else {
@@ -68,8 +68,6 @@ function totalPoints(max){
     }
     return 0;
 }
-
-
 
 // funzione per controllare se un numero è gia presente in un array
 function isRepeated(a, element){
@@ -83,8 +81,6 @@ function isRepeated(a, element){
     }
     return check;
 }
-
-
 
 // funzione per generare numeri random
 function getRandom(min, max) {
